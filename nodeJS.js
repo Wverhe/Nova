751f357bc007913570c2a0325927d8198d08cf25
Wverhe <jetmaster12345@yahoo.com>
2018-03-14 17:30:56 -0500

    Initial Commit with Bootstrap
    Trying to fix git conflicts with source tree aswell

--- nodeJS.js
+++ nodeJS.js
@@ -6,38 +6,7 @@ app.use(express.static(__dirname + '/resources/assets'));
 app.use(express.static(__dirname + '/resources/styling'));
 app.use(express.static(__dirname + '/resources/functionality'));
 
-
-const db_config = {
-	host: "localhost",
-	user: "root",
-	password: "@Gtmp1002_0116",
-	database: "NOVA"
-};
-
-var con;
-
-function handleDisconnect(){
-	con = mysql.createConnection(db_config);
-	con.connect(function(err){
-		if(err){
-			console.log("Error when connection to db:", err);
-			setTimeout(handleDisconnect, 5000);
-		}
-	});
-	
-	con.on("error", function(err){
-		console.log("DB Error", err);
-		if(err.code == "PROTOCOL_CONNECTION_LOST"){
-			handleDisconnect();
-		} else {
-			throw err;
-		}
-	});
-}
-
 app.listen(80, () => console.log('Server running on Port 80'));
-
-handleDisconnect();
 
 function getParameterByName(name, url) {
 	if (!url) url = window.location.href;
--- resources/functionality/siteFunctionality.js
+++ resources/functionality/siteFunctionality.js
@@ -1,29 +1,3 @@
-//Displays Nav Menu
-function showMenu(){
-	if($("#menu").css("display") == "none"){		
-		$("#menu").css("display", "flex").hide().fadeIn("slow");
-	}
-	// Find a way to load these through another function //
-	document.getElementById("menu").onclick = function(event){
-		if(event.target == document.getElementById('menu') || event.target == document.getElementById("closeNav")){
-			closeNavMenu();
-		}
-	}
-	
-	$(".closeNav").mouseover(function(){
-		$(this).find("i").addClass("fa-lg");
-	});
-
-	$(".closeNav").mouseout(function(){
-		$(this).find("i").removeClass("fa-lg");
-	});
-}
-
-function closeNavMenu(){
-	$("#menu").fadeOut("slow");
-	//document.getElementById("menu").style.visibility = "hidden";
-}
-
 function getParameterByName(name, url) {
 	if (!url) url = window.location.href;
 	name = name.replace(/[\[\]]/g, "\\$&");
